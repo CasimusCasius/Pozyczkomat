81232b0cb1eac3f3b3e1d097c4e4599c367b3676
Grzegorz Kazimierczak <grzegorz.kazimierczak.sg@gmail.com>
2019-04-11 16:32:47 +0200

    Dodaj pliki projektów.

---
+++ Pozyczkomat/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Pozyczkomat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.joesCashLabel = new System.Windows.Forms.Label();
+            this.bobsCashLabel = new System.Windows.Forms.Label();
+            this.bankCashLabel = new System.Windows.Forms.Label();
+            this.btnGiveCash = new System.Windows.Forms.Button();
+            this.btnRecieveCash = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // joesCashLabel
+            // 
+            this.joesCashLabel.AutoSize = true;
+            this.joesCashLabel.Location = new System.Drawing.Point(12, 9);
+            this.joesCashLabel.Name = "joesCashLabel";
+            this.joesCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.joesCashLabel.TabIndex = 0;
+            this.joesCashLabel.Text = "label1";
+            // 
+            // bobsCashLabel
+            // 
+            this.bobsCashLabel.AutoSize = true;
+            this.bobsCashLabel.Location = new System.Drawing.Point(12, 45);
+            this.bobsCashLabel.Name = "bobsCashLabel";
+            this.bobsCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.bobsCashLabel.TabIndex = 1;
+            this.bobsCashLabel.Text = "label2";
+            // 
+            // bankCashLabel
+            // 
+            this.bankCashLabel.AutoSize = true;
+            this.bankCashLabel.Location = new System.Drawing.Point(11, 81);
+            this.bankCashLabel.Name = "bankCashLabel";
+            this.bankCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.bankCashLabel.TabIndex = 2;
+            this.bankCashLabel.Text = "label3";
+            // 
+            // btnGiveCash
+            // 
+            this.btnGiveCash.Location = new System.Drawing.Point(42, 122);
+            this.btnGiveCash.Name = "btnGiveCash";
+            this.btnGiveCash.Size = new System.Drawing.Size(118, 23);
+            this.btnGiveCash.TabIndex = 3;
+            this.btnGiveCash.Text = "Daj 10 zł Joemu";
+            this.btnGiveCash.UseVisualStyleBackColor = true;
+            this.btnGiveCash.Click += new System.EventHandler(this.btnGiveCash_Click);
+            // 
+            // btnRecieveCash
+            // 
+            this.btnRecieveCash.Location = new System.Drawing.Point(287, 122);
+            this.btnRecieveCash.Name = "btnRecieveCash";
+            this.btnRecieveCash.Size = new System.Drawing.Size(118, 23);
+            this.btnRecieveCash.TabIndex = 4;
+            this.btnRecieveCash.Text = "Weź 5 zł od Boba";
+            this.btnRecieveCash.UseVisualStyleBackColor = true;
+            this.btnRecieveCash.Click += new System.EventHandler(this.btnRecieveCash_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(287, 170);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 229);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRecieveCash);
+            this.Controls.Add(this.btnGiveCash);
+            this.Controls.Add(this.bankCashLabel);
+            this.Controls.Add(this.bobsCashLabel);
+            this.Controls.Add(this.joesCashLabel);
+            this.Name = "Form1";
+            this.Text = "Zabawa z Joe i Bobem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label joesCashLabel;
+        private System.Windows.Forms.Label bobsCashLabel;
+        private System.Windows.Forms.Label bankCashLabel;
+        private System.Windows.Forms.Button btnGiveCash;
+        private System.Windows.Forms.Button btnRecieveCash;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Pozyczkomat/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pozyczkomat
+{
+    public partial class Form1 : Form
+    {
+        Guy Joe;
+        Guy Bob;
+        int bank = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Joe = new Guy() { Cash = 50, Name = "Joe" };
+            Bob = new Guy() { Name = "Bob", Cash = 100 };
+            UpdateForm();
+        }
+
+        private void btnGiveCash_Click(object sender, EventArgs e)
+        {
+            if (bank >= 10)
+            {
+                bank -= Joe.RecieveCash(10);
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("Bank nie posiada takiej ilości pieniędzy");
+            }
+        }
+
+        private void btnRecieveCash_Click(object sender, EventArgs e)
+        {
+            bank -= Bob.GiveCash(5);
+            UpdateForm();
+        }
+
+        public void UpdateForm()
+        {
+            joesCashLabel.Text = Joe.Name + " ma " + Joe.Cash + " zł";
+            bobsCashLabel.Text = Bob.Name + " ma " + Bob.Cash + " zl";
+            bankCashLabel.Text = "Bank ma " + bank + " zł";
+        }
+    }
+}
